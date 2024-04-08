4fa2730315c79ec1d68d1e6f5d3a9b377ab378f3
Devadavid <s3927112@rmit.edu.vn>
2024-04-08 21:30:55 +0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "organization-name"
+#   cloud {
+#     organization = "khangdev"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "learn-terraform-cloud"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.31.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.31.0"
+#     }
+#   }
 
-  required_version = "~> 1.2"
-}
+#   required_version = "~> 1.2"
+# }
